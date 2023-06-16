ef6e53e735426296923d47ceb8e6dae728e3192c
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2023-06-16 10:54:25 +0530

    [feat] anagram problem solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,44 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const strOneArr = str1.split("").map((letter) => letter.toUpperCase());
+  const strTwoArr = str2.split("").map((letter) => letter.toUpperCase());
+  const letterCount = {};
+  const strTwoLetterCount = {};
+  if (strOneArr.length === strTwoArr.length) {
+    for (let i = 0; i < strOneArr.length; i++) {
+      const crrLetterCount = letterCount[strOneArr[i]];
+      if (crrLetterCount) {
+        letterCount[strOneArr[i]]++;
+      } else {
+        letterCount[strOneArr[i]] = 1;
+      }
+    }
+    for (let i = 0; i < strTwoArr.length; i++) {
+      const crrLetterCount = strTwoLetterCount[strTwoArr[i]];
+      if (crrLetterCount) {
+        strTwoLetterCount[strTwoArr[i]]++;
+      } else {
+        strTwoLetterCount[strTwoArr[i]] = 1;
+      }
+    }
+    const letterArr = Object.keys(letterCount);
+    for (let i = 0; i < letterArr.length; i++) {
+      const crrLetter = letterArr[i];
+      if (strTwoLetterCount[crrLetter]) {
+        if (letterCount[crrLetter] !== strTwoLetterCount[crrLetter]) {
+          return false;
+          break;
+        }
+      } else {
+        return false;
+        break;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
